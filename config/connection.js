73c17ad950eaaa8473d52e7d5dc957bb59edda22
Alexia Chalita <chalitaalexia@gmail.com>
2020-06-07 20:12:03 -0700

    new

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,19 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
+if (process.env.JAWSDB_URL) {
+connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+ connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
-  password: "abc123",
+  password: "root",
   database: "hamburgers_db",
   insecureAuth : true
 });
-
+}
 // Make connection.
 connection.connect(function(err) {
   if (err) {
